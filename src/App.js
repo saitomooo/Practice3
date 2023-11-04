a166aa982d35d8ec898e8fae2b95fab430e5da6a
saitomooo <tamaapples@gmail.com>
2023-11-04 14:20:59 +0900

    BoardにUsestateを導入＆引継ぎ

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 function Square() {
   const [value, setValue] = useState(null);
   function handleClick() {
-    console.log('clicked!');
+    setValue('X');
   }
 
   return (
@@ -15,22 +15,23 @@ function Square() {
   );
 }
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
   return (
     <>
      <div className="board-row">
-        <Square  />
-        <Square  />
-        <Square  />
+        <Square  value={squares[0]}/>
+        <Square  value={squares[1]}/>
+        <Square  value={squares[2]}/>
       </div>
       <div className="board-row">
-        <Square  />
-        <Square  />
-        <Square  />
+        <Square  value={squares[3]}/>
+        <Square  value={squares[4]}/>
+        <Square  value={squares[5]}/>
       </div>
       <div className="board-row">
-        <Square  />
-        <Square  />
-        <Square  />
+        <Square  value={squares[6]}/>
+        <Square  value={squares[7]}/>
+        <Square  value={squares[8]}/>
       </div>
     </>
   );
