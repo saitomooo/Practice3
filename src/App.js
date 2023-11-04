547919b31c7fb81560b24392cd2d5d37760d1e36
saitomooo <tamaapples@gmail.com>
2023-11-04 13:46:01 +0900

    コンソールをsquareに追加

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
 import { useState } from 'react';
 function Square({value}) {
-  return <button className="square">{value}</button>;
+  function handleClick() {
+    console.log('clicked!');
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
 }
 export default function Board() {
   return (
